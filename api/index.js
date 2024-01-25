2350d8b9986568fe7f2fae5a179ce27719e39e87
Sbusiso Macu <macumog@gmail.com>
2024-01-25 13:34:15 +0200

    connecting mongoDB

--- api/index.js
+++ api/index.js
@@ -1,10 +1,36 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
-const app = use(cors());
+const { default: mongoose } = require('mongoose');
+const User = require('./models/User');
 
-app.get('/test', (req,res) => {
-    res.json('test ok sure');
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+
+//Connecting RegisterPage.js to the database
+mongoose.connect('mongodb+srv://blog:Sbuda.211@cluster0.4motcbu.mongodb.net/mern-blog?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+    console.log('Connected to MongoDB');
+})
+
+
+app.post('/register', (req, res) => {
+  const { username, password } = req.body;
+  res.json({requestData:{username,password}});
 });
 
-app.listen(4000)
+const PORT = process.env.PORT || 4002;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port 4002`);
+});
+//Sbuda.211
+//mongodb+srv://blog:Sbuda.211@cluster0.4motcbu.mongodb.net/?retryWrites=true&w=majority
